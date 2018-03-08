20243dade43abee0b61ce914df1d811d09a04716
qiuhaoxin <502967244@qq.com>
2018-03-09 00:28:43 +0800

    commit

--- js/chatlist.js
+++ js/chatlist.js
@@ -19,98 +19,100 @@
     var socket=null;
     var scrollerH=0;
     var realScrollH=0;
+    var $list=$('#list');
     function ChatList(options){
          console.log("init chatlist");
          this.$btn=getObjById('submit');//发送按钮
          $listContainer=getObjById('list');//消息容器
          $input=document.querySelector(".footer input");
-         socket=new Socket("ws://172.20.71.86:8888/rest/ws/api/test");
-	     socket.setEventCallBack("onmessage",this.acceptMsg.bind(this))
-         socket.open();
+         //socket=new Socket("ws://172.20.71.86:8888/rest/ws/api/test");
+	     //socket.setEventCallBack("onmessage",this.acceptMsg.bind(this))
+         //socket.open();
          this.init();
     }
-    //页面初始化
-    ChatList.prototype.init=function(){
-    	var _this=this;
-    	scrollerH=document.body.clientHeight - 44;
-    	console.log("scrollerH is "+scrollerH);
-        if(this.$btn){
-        	this.bindEvent(this.$btn,'click',this.sendMessage.bind(this))
-        }
-        if($input){
-        	this.bindEvent($input,"keyup",function(e){
-                 e.preventDefault();
-                 if(e.keyCode==13){
-                 	_this.sendMessage();
-                 }
-        	})
-        }
-    }
-    //接收服务器返回的信息
-    ChatList.prototype.acceptMsg=function(data){
-       var LI=document.createElement('LI');
-       LI.classList.add('msg-item');
-       //<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>'
-       //var inerText='<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>';
-       var inerText='<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + data + '</div>'
-       LI.innerHTML=inerText;
-       $listContainer.appendChild(LI);
-       this.scroll();
+    ChatList.prototype={
+        init:function(){
+            var _this=this;
+            scrollerH=document.body.clientHeight - 44;
+            console.log("scrollerH is "+scrollerH);
+            if(this.$btn){
+                this.bindEvent(this.$btn,'click',this.sendMessage.bind(this))
+            }
+            if($input){
+                this.bindEvent($input,"keyup",function(e){
+                     e.preventDefault();
+                     if(e.keyCode==13){
+                        _this.sendMessage();
+                     }
+                })
+            }
+        },
+        acceptMsg:function(data){
+            data="test";
+            var LI=document.createElement('LI');
+            LI.classList.add('msg-item');
+            //<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + Fake[i] + '</div>'
+            //var inerText='<div class="message loading new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure><span></span></div>';
+            var inerText='<div class="message new"><figure class="avatar"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/156381/profile/profile-80.jpg" /></figure>' + data + '</div>'
+            LI.innerHTML=inerText;
+            $listContainer.appendChild(LI);
+            this.scroll();
 
-    }
-    ChatList.prototype.scroll=function(){
-    	this.caculate();
-    	console.log("ehi");
-    	$listContainer.scrollLeft=20;
-    	if(realScrollH > scrollerH){
-    		var scrollTop=scrollerH - realScrollH;
-    		 console.log("should scroll pas "+scrollTop);
-    		 //$listContainer.scrollTop=(realScrollH-scrollerH);
-    		 $listContainer.scrollLeft=20;
-    		 console.log("scroll Top is "+$listContainer.scrollTop);
-    		//$listContainer.style['-webkit-transform']="translate3d(0px,"+scrollTop+"px,0px)";
-    	}
-    }
-    ChatList.prototype.caculate=function(){
-    	realScrollH=$listContainer.scrollHeight;
-    }
-    ChatList.prototype.clearInput=function(){
-    	$input.value="";
-    }
-    ChatList.prototype.sendMessage=function(e){
-    	var inputVal=$input.value;
-    	 if(isEmpty(inputVal)){
-    	 	return;
-    	 } 
-         this.insertMsg(0,inputVal);
-    	 socket.send(inputVal);
-    	 //清空输入框
-    	 this.clearInput();
-    	 this.scroll();
-    }
-    ChatList.prototype.setDate=function(){
-    	var d=new Date();
-    	var timeDIV=document.createElement('DIV');
-    	timeDIV.innerHTML=d.getHours()+":"+d.getMinutes();
-    	return timeDIV;
-    }
+        },
+        scroll:function(){
+            this.caculate();
+            //$listContainer.scrollLeft=20;
+            console.log("realScrollH is "+$list[0].scrollHeight);
+            //$listContainer.style['top']="20px";
+            if(realScrollH > scrollerH){
+                var scrollTop=scrollerH - realScrollH;
+                 console.log("should scroll pas "+realScrollH);
+                 //$listContainer.scrollTop=(realScrollH-scrollerH);
+                 //$listContainer.scrollLeft=20;
+                 $list[0].scrollTop=$list[0].scrollHeight;
+            }
+        },
+        caculate:function(){
+            realScrollH=$listContainer.scrollHeight;
+        },
+        clearInput:function(){
+            $input.value="";
+        },
+        sendMessage:function(e){
+            var inputVal=$input.value;
+             if(isEmpty(inputVal)){
+                return;
+             } 
+             this.insertMsg(0,inputVal);
+             //socket.send(inputVal);
+             //清空输入框
+             this.clearInput();
+               this.acceptMsg();
+             this.scroll();
+        },
+        setDate:function(){
+            var d=new Date();
+            var timeDIV=document.createElement('DIV');
+            timeDIV.innerHTML=d.getHours()+":"+d.getMinutes();
+            return timeDIV;
+        },
     //显示消息到界面  type:用户问（显示在界面左边）：0  系统答(内容显示在界面右边):1  ,content:消息内容
-    ChatList.prototype.insertMsg=function(type,content){
-       var LI=document.createElement('LI');
-       LI.classList.add('msg-item');
-       var MSG=document.createElement('DIV');
-       MSG.classList.add('message');
-       MSG.classList.add('message-personal');
-       MSG.innerHTML=content;
-       LI.appendChild(MSG);
-       $listContainer.appendChild(LI);
+        insertMsg:function(type,content){
+           var LI=document.createElement('LI');
+           LI.classList.add('msg-item');
+           var MSG=document.createElement('DIV');
+           MSG.classList.add('message');
+           MSG.classList.add('message-personal');
+           MSG.innerHTML=content;
+           LI.appendChild(MSG);
+           $listContainer.appendChild(LI);
+        },
+        bindEvent:function(obj,eventName,callback){
+           obj.addEventListener(eventName,callback,false);
+        },
+        unbindEvent:function(obj,eventName,callback){
+           obj.removeEventListener(eventName,callback,false);
+        }
     }
-    ChatList.prototype.bindEvent=function(obj,eventName,callback){
-        obj.addEventListener(eventName,callback,false);
-    }
-    ChatList.prototype.unbindEvent=function(obj,eventName,callback){
-    	obj.removeEventListener(eventName,callback,false);
-    }
-
     return ChatList;
 })
